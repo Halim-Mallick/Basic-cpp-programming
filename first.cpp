0de2d69043122e6b517959494c8db4f17b551669
Halim Mallick <113496380+Halim-Mallick@users.noreply.github.com>
2023-02-10 00:01:56 +0600

    version 1.0

---
+++ first.cpp
@@ -0,0 +1,15 @@
+//My first C++ programming
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num1=10,num2=20;
+    char A='a';
+    cout<<"Hello World" <<endl;
+    cout<<"Number one is:"<< num1 <<endl;
+    cout<< "number two is:"<< num2 <<endl;
+    cout<< "Character is:"<< A <<endl;
+
+
+    return 0;
+}

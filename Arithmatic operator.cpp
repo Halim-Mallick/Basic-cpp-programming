797ba969bb4bac9b6ca1cc128d68f223897917df
Halim Mallick <113496380+Halim-Mallick@users.noreply.github.com>
2023-02-10 01:19:29 +0600

    1.0

---
+++ Arithmatic operator.cpp
@@ -0,0 +1,16 @@
+//Arithmatic operator
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int a=30,b=20;
+
+    cout<<"A+B is:"<<(a+b) <<endl;
+    cout<<"A-B is:"<<(a-b) <<endl;
+    cout<<"A*B is:"<<(a*b) <<endl;
+    cout<<"A/B is:"<<(a/b) <<endl;
+    cout<<"A%B is:"<<(a%b) <<endl;
+
+    return 0;
+}

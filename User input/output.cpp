f6d92a86c9690bd8a103bf8fe2d32aa96fadd0de
Halim Mallick <113496380+Halim-Mallick@users.noreply.github.com>
2023-02-10 00:12:05 +0600

    1.0

---
+++ User input/output.cpp
@@ -0,0 +1,19 @@
+//input output
+#include<iostream>
+using namespace std;
+
+int main(){
+    double num;
+    char ch;
+
+    cout<<"Enter A double number: ";
+    cin>> num;
+    cout<<"Enter A character:";
+    cin>> ch;
+
+    cout<<"Double number is:"<<num <<endl;
+    cout<<"Character is:"<<ch <<endl;
+
+
+    return 0;
+}

913003096f0666cd6fc437f9715bc4e21be53fc7
Halim Mallick <113496380+Halim-Mallick@users.noreply.github.com>
2023-02-10 21:02:00 +0600

    1.0

---
+++ function/Square Root of a Number.cpp
@@ -0,0 +1,18 @@
+// Find the Square Root of a Number
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main(){
+    float squareRoot;
+    int num;
+
+    cout<<"Enter a number: ";
+    cin>> num;
+
+    squareRoot=sqrt(num);
+
+    cout<<"Square Root is= "<<squareRoot;
+
+    return 0;
+}
